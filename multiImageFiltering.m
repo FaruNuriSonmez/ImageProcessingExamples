271099fe1f19410aea3a98ea9e52a6b9781b0072
Faru Nuri Sönmez <info@farunurisonmez.com>
2020-11-08 15:22:09 +0300

    Multiple Image Filtering
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- multiImageFiltering.m
+++ multiImageFiltering.m
@@ -7,8 +7,8 @@ N = length(imagefiles) ;  % Total number of image files
 for i = 1:N  % loop for each file 
     currentfilename = imagefiles(i).name;
     currentimage = imread(currentfilename);
-    type = fspecial('motion');
-    motion = imfilter(currentimage,type,'replicate');
+    hMotionFilter = fspecial('motion',50,45);
+    motion = imfilter(currentimage,hMotionFilter,'replicate');
     subplot(3,3,i);
     imshow(motion); 
     title('Motion Filter','FontSize',15);
