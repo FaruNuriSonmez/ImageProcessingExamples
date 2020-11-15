b0138dd6d044c250e88c5ddd7bd6935683b25f2a
Faru Nuri Sönmez <info@farunurisonmez.com>
2020-11-16 00:12:22 +0300

    Noise Filters
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ noiseFilters.m
@@ -0,0 +1,30 @@
+clear; close all;
+
+img = imread('markerFrame.tif'); % Image Uploaded.
+img=im2double(img); % Converted to double type.
+
+subplot(2,3,1); 
+imshow(img); 
+title('Original Image','FontSize',15);
+
+subplot(2,3,2); 
+gaussianFilter=imnoise(img,'gaussian',0.30);
+imshow(gaussianFilter); 
+title('Gaussian Filter','FontSize',15);
+
+subplot(2,3,3); 
+poissonFilter=imnoise(img,'poisson');
+imshow(poissonFilter);
+title('Poisson Filter','FontSize',15);
+
+subplot(2,3,4); 
+saltpepperFilter = imnoise(img,'salt & pepper',0.02);
+imshow(saltpepperFilter);
+title('Salt & Pepper Filter','FontSize',15);
+
+subplot(2,3,5); 
+speckleFilter = imnoise(img,'speckle');
+imshow(speckleFilter);
+title('Speckle Filter','FontSize',15);
+
+
